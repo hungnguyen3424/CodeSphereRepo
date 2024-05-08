c9f6cbf18989b4089a19033b097b2109d1e96ba6
hungnguyen3424 <kahlid2010nam1311@outlook.com>
2024-05-08 21:28:27 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-function searchMatrix(matrix, target) {
-  if (matrix.length === 0 || matrix[0].length === 0) return false;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  let row = 0;
-  let col = cols - 1;
-  while (row < rows && col >= 0) {
-    if (matrix[row][col] === target) return true;
-    else if (matrix[row][col] < target) row++;
-    else col--;
-  }
-  return false;
+function largestNumber(nums) {
+  if (nums.every((num) => num === 0)) return "0";
+  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
 }
