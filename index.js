cd8e392e35dcd14d7c97dde70ac6d7285c53a4ac
hungnguyen3424 <kahlid2010nam1311@outlook.com>
2024-05-12 04:57:59 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function getMax(arr) {
-  return Math.max(...arr);
+function capitalizeWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
